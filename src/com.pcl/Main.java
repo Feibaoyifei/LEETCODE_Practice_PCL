a9b8e6c4782a2a1f066c961244c3d31712f52154
Crystal <17854225857@163.com>
2020-12-23 16:44:45 +0800

    "two num"

---
+++ src/com.pcl/Main.java
@@ -0,0 +1,12 @@
+package com.pcl;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        //public int[] twoSum(int[] nums, int target)
+        int[] a ={2,7,11,15};
+        int num=9;
+        System.out.println(a[1]);
+
+    }
+}
---
+++ src/com.pcl/twoNum.java
@@ -0,0 +1,21 @@
+package com.pcl;
+
+public class twoNum {
+    int[] res = new int[2];
+
+    int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+
+            }
+
+        }
+        return res;
+    }
+
+}
