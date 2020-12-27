e0a68901ba4823f9620a8e0f96e15cdeba1b8c4e
Crystal <17854225857@163.com>
2020-12-27 20:12:40 +0800

    "两数字之和"

--- src/com/pcl/leetcode/easy/twoNum.java
+++ src/com/pcl/leetcode/easy/twoNum.java
@@ -2,9 +2,7 @@ package com.pcl.leetcode.easy;
 
 public class twoNum {
     int[] res = new int[2];
-
     int[] twoSum(int[] nums, int target) {
-
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
