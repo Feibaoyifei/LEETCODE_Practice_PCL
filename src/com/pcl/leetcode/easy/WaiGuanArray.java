30febad315390eed893f911b295bb5c8f4c9e07a
Crystal <17854225857@163.com>
2021-01-09 10:44:20 +0800

    "外观数列，性能极差，虽能pass"

---
+++ src/com/pcl/leetcode/easy/WaiGuanArray.java
@@ -0,0 +1,53 @@
+package com.pcl.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class WaiGuanArray {
+    public String say(String str){
+        int len=str.length();
+        char[] ch=str.toCharArray();
+
+        char[][] arr=new char[30][2];//此处应设置char[10000][2],起初设置为[30][2]报错数组越界。后改为100，1000，直到一万
+        StringBuilder sb=new StringBuilder();
+        arr[0][0]=ch[0];
+        arr[0][1]='1';
+        int top=0;
+        for(int i=1;i<len;i++){
+            if(ch[i]==arr[top][0])
+                arr[top][1]++;
+            else {
+                arr[++top][0]=ch[i];
+                arr[top][1]='1';
+            }
+
+        }
+
+        for(int i=0;i<=top;i++){
+            sb.append(arr[i][1]).append(arr[i][0]);
+
+        }
+
+
+
+
+        return sb.toString();
+    }
+
+
+      public String countAndSay(int n) {
+
+              if(n==1)
+                  return "1";
+              else  return  say(countAndSay(n-1));
+
+
+
+
+          }
+
+
+
+
+}
