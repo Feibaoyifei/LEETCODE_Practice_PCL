b55a524c510c53e0df8b6098503b8dda3bfcdc8d
Crystal <17854225857@163.com>
2021-01-09 22:15:10 +0800

    "最后一个单词的长度"

--- src/com/pcl/leetcode/easy/LengthOfLastWord.java
+++ src/com/pcl/leetcode/easy/LengthOfLastWord.java
@@ -3,12 +3,22 @@ package com.pcl.leetcode.easy;
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
 
+        /**此方法效率较慢
         String[] str=s.split("\\s+ ");
         int n=str.length;
         if(n==0) return 0;
         else
-        return str[n-1].length();
+        return str[n-1].length();*/
 
+        //此方法效率高，直接从字符串末尾数数出来最后一个单词的长度
+        int length=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.indexOf(i)!=' ')
+                length++;
+            else if(length!=0)
+                return length;
+        }
 
+return length;
     }
 }
